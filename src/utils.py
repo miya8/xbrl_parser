d593e7cf3d9e58673cc186a4a0d04ee8813f3e03
miya8 <e-miyasaka@hotmail.co.jp>
2020-07-17 19:32:11 +0900

    generalize extract_files_from_zip function

--- src/utils.py
+++ src/utils.py
@@ -3,24 +3,34 @@ import os
 import re
 import zipfile
 
-def extract_files_from_zip(zip_dir):
-    """zipファイルから必要なファイルを抽出する"""
 
-    unzip_tgt_regrep = "|".join(
-        [f"XBRL/PublicDoc/.*\.{extension}" for extension in ["xbrl", "xsd", "xml"]]
-    )
-    zip_files = glob.glob(os.path.join(zip_dir, '*.zip'))
+def extract_files_from_zip(zip_dir, tgt_zfile_names=None, dest_dir_root=None, dest_dirname=None, unzip_members_regrep=None):
+    """zipファイルからファイルを抽出する"""
+
+    if tgt_zfile_names is None:
+        zip_files = glob.glob(os.path.join(zip_dir, "*.zip"))
+    else:
+        zip_files = [os.path.join(zip_dir, fname) for fname in tgt_zfile_names]
     zip_file_num = len(zip_files)
     for index, zip_file in enumerate(zip_files):
         zfile_name = os.path.splitext(os.path.basename(zip_file))[0]
         print(f"{zfile_name}: {index + 1} / {zip_file_num}")
+        if dest_dirname is None:
+            dest_last_dir = zfile_name
+        if dest_dir_root is None:
+            dest_dir_path = os.path.join(zip_dir, dest_last_dir)
+        else:
+            dest_dir_path = os.path.join(dest_dir_root, dest_last_dir)
         with zipfile.ZipFile(zip_file) as zf:
-            tgt_files_namelist = []
-            for filename in zf.namelist():
-                if re.findall(unzip_tgt_regrep, filename):
-                    tgt_files_namelist.append(filename)
+            if unzip_members_regrep is None:
+                tgt_members_names = None
+            else:
+                tgt_members_names = []
+                for filename in zf.namelist():
+                    if re.findall(unzip_members_regrep, filename):
+                        tgt_members_names.append(filename)
             zf.extractall(
-                path=os.path.join(EDINET_ROOT_DIR, zfile_name),
-                members=tgt_files_namelist
+                path=dest_dir_path,
+                members=tgt_members_names
             )
             zf.close()
