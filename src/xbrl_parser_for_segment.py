74fb1cf2d7acf2c103fccbca55aae1496b6433d7
miya8 <e-miyasaka@hotmail.co.jp>
2020-07-28 13:36:33 +0900

    add sample code using dimension

---
+++ src/xbrl_parser_for_segment.py
@@ -0,0 +1,199 @@
+import glob
+import os
+import re
+import sys
+import zipfile
+
+import pandas as pd
+
+from arelle import Cntlr, ModelManager, XbrlConst
+from arelle.ModelValue import qname
+from edinetcd_info import get_edinetcd_info
+from utils import extract_files_from_zip
+
+# パス関連
+EDINET_ROOT_DIR = "D:\\EDINET\\140_qr_from20200501to20200630_test"
+EDINET_XBRL_REGREX = "*\\XBRL\\PublicDoc\\*.xbrl"
+OUTPUT_FILE_NAME = "qr_segment_info.csv"
+
+# EDINETからダウンロードしたXBRLを含むzipファイルが解凍済かどうか
+IS_EXTRACTED = True
+
+# ----- 財務情報XBRLから取得する内容 -----
+# EDINETコードを示す要素
+EDINET_CD_ELM_NAME = "EDINETCodeDEI"
+# 連結有無を示す要素
+HAS_CONSOLIDATED_ELM_NAME = "WhetherConsolidatedFinancialStatementsArePreparedDEI"
+# 提出書類の様式を示す要素
+DOC_TYPE_ELM_NAME = "DocumentTypeDEI"
+
+# 取得対象のDEI（会社・書類情報）
+# - 今回欲しい以下の項目は全企業登録必須のため、Qname（名前空間:要素名）指定で取得する
+#   - 必須項目について
+#     - EDINETバリデーションガイドライン: DEI 必須項目　参照
+#   - 要素名について
+#     - タクソノミ要素リスト: DEI  (jpdei)　参照
+DEI_COLS = [
+    DOC_TYPE_ELM_NAME,
+    EDINET_CD_ELM_NAME,
+    HAS_CONSOLIDATED_ELM_NAME,
+    "AccountingStandardsDEI",
+    "SecurityCodeDEI",
+    "FilerNameInJapaneseDEI",
+    "CurrentPeriodEndDateDEI",
+    "CurrentFiscalYearEndDateDEI"
+]
+# 【備考】財務諸表本表の項目は企業ごとに項目が異なるため、
+# リンクベースに沿って情報を取得する
+
+# ----- EDINETコードリストから取得する列 -----
+EDINETCD_COL = "ＥＤＩＮＥＴコード"
+EDINETCDDLINFO_COLS = [
+    EDINETCD_COL,
+    "提出者業種",
+    "上場区分",
+    "提出者種別",
+    "提出者名"
+]
+
+
+def get_segments_facts(model_xbrl):
+    """XBRLデータから損益計算書の第一階層の勘定科目の値を取得する"""
+
+    qname_prefix = "jpcrp_cor"
+    ns = model_xbrl.prefixedNamespaces[qname_prefix]
+    dict_facts = {}
+
+    # 【備考】factsByDimMemQnameメソッドで、
+    # 指定したDimensionが設定されているfactを取得できる。
+    # ここではセグメントを示すDimensionを指定する。
+    tgt_dimension = qname(
+        model_xbrl.prefixedNamespaces[qname_prefix],
+        name=f"{qname_prefix}:OperatingSegmentsAxis"
+    )
+    facts_by_dim = model_xbrl.factsByDimMemQname(tgt_dimension)
+    if not facts_by_dim:
+        print("セグメントなし")
+        return None
+    for fact in facts_by_dim:
+        # セグメント情報のDimensionメンバーのラベルを列、各要素を行とするデータを作成する
+        # 【備考】Dimensionメンバーのラベル取得方法
+        # 1. fact.context.dimValue(tgt_dimension) 
+        #    - factに設定されているセグメントを示すDimensionの値
+        #      （ModelDimensionValueクラスのインスタンス）を取得
+        #      ここではセグメントのDimensionを扱っているので、例えば
+        #     「商業施設部門」「チェーンストア部門」などセグメントの種類を示す。
+        # 2. .member
+        #    - ModelDimensionValueクラスのインスタンスの属性の１つで
+        #      ModelConceptクラスのインスタンス
+        # 3. .label()
+        #    - 2に設定されているラベルを取得
+        dim_mem_label = fact.context.dimValue(tgt_dimension).member.label()
+        # 日本円のfactのみ取得する
+        if fact.unitID == "JPY":
+            fact_label = fact.concept.label()
+            end_date = fact.context.endDatetime.strftime("%Y%m%d")
+            if not fact_label in dict_facts.keys():
+                dict_facts[fact_label] = {}
+            if not end_date in dict_facts[fact_label]:
+                dict_facts[fact_label][end_date] = {}
+            dict_facts[fact_label][end_date][dim_mem_label] = fact.value
+    df_facts = pd.json_normalize(dict_facts.values())
+    df_facts.sort_index(axis=1, inplace=True)
+    cols = df_facts.columns.tolist()
+    df_facts["セグメント情報"] = dict_facts.keys()
+    df_facts = df_facts[["セグメント情報"]+cols]
+    return df_facts
+
+
+def get_dei_facts(model_xbrl):
+    """XBRLデータから会社・書類情報を取得する"""
+
+    qname_prefix = "jpdei_cor"
+    ns = model_xbrl.prefixedNamespaces[qname_prefix]
+    dict_facts = {}
+
+    # 【備考】: Qname指定でfactを取得
+    # ModelXbrlクラスのインスタンスのfactsBy*属性（辞書型）にfactが格納されている。
+    # このうち、QnameをキーとするfactsByQnameを使用する。
+    # 但し、factsByQnameのキーはQname文字列ではなく、Qnameクラスのインスタンス。
+    # 文字列からQnameインスタンスを作成するために、qname関数を使用する。
+    # qname関数の引数には名前空間uriが必要であるため、
+    # ModelXbrlクラスのprefixedNamespaces属性（辞書型）から取得。
+    for localname in DEI_COLS:
+        facts = model_xbrl.factsByQname[qname(
+            ns, name=f"{qname_prefix}:{localname}")]
+        if (not facts) or (len(facts) > 1):
+            print(f"【想定外】1つのXBRL内に{qname_prefix}:{localname}のfactが複数存在します。")
+            sys.exit()
+        fact = list(facts)[0]
+        if localname == DOC_TYPE_ELM_NAME:
+            if fact.value != "第四号の三様式":
+                return None
+        if localname == EDINET_CD_ELM_NAME:
+            dict_facts[EDINETCD_COL] = fact.value
+        else:
+            dict_facts[fact.concept.label()] = fact.value
+
+    return pd.DataFrame([dict_facts])
+
+
+def get_facts(xbrl_file):
+    """有価証券報告書から情報を取得する"""
+
+    ctrl = Cntlr.Cntlr()
+    model_manager = ModelManager.initialize(ctrl)
+    model_xbrl = model_manager.load(xbrl_file)
+
+    # セグメント情報を取得
+    df_facts_segment = get_segments_facts(model_xbrl)
+    if df_facts_segment is None:
+        return None
+
+    # 会社・書類情報を取得
+    df_facts_dei = get_dei_facts(model_xbrl)
+    if df_facts_dei is None:
+        return None
+
+    # マージ
+    df_facts_segment.loc[:, "key"] = 1
+    df_facts_dei.loc[:, "key"] = 1
+    df_facts = df_facts_dei.merge(df_facts_segment, on="key", how="left").drop(columns=["key"])
+
+    return df_facts
+
+
+def main():
+    if IS_EXTRACTED:
+        pass
+    else:
+        edinet_zip_dir = os.path.join(EDINET_ROOT_DIR, "zip")
+        extract_files_from_zip(
+            edinet_zip_dir,
+            dest_dir_root=EDINET_ROOT_DIR,
+            unzip_members_regrep="|".join(
+                [f"XBRL/PublicDoc/.*\.{extension}" for extension in ["xbrl", "xsd", "xml"]]
+            )
+        )
+    # XBRLから情報取得
+    xbrl_file_regrex = os.path.join(EDINET_ROOT_DIR, EDINET_XBRL_REGREX)
+    xbrl_files = glob.glob(xbrl_file_regrex)
+    list_df_facts = []
+    for index, xbrl_file in enumerate(xbrl_files):
+        print(xbrl_file, ":", index + 1, "/", len(xbrl_files))
+        list_df_facts.append(get_facts(xbrl_file))
+    df_xbrl = pd.concat(list_df_facts, axis=0, sort=False)
+    # Edinetコードリストの情報をマージ
+    df_edinetcd_info = get_edinetcd_info(EDINETCDDLINFO_COLS)
+    df_xbrl = df_edinetcd_info.merge(df_xbrl, on=EDINETCD_COL, how="right")
+
+    df_xbrl.to_csv(
+        os.path.join(EDINET_ROOT_DIR, OUTPUT_FILE_NAME),
+        index=False,
+        encoding="cp932"
+    )
+    print(f"{'-'*10} 情報抽出　完了 {'-'*10}")
+
+
+if __name__ == "__main__":
+    main()
